6e90d0b6a0922c95922a7955fd60f0d4de4d0e40
Harrison Smith <harrison@econ1-130-29-dhcp.int.colorado.edu>
2024-11-11 13:54:13 -0700

    im done for now :(

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+import * as THREE from 'three';
+import "./style.css"
+import gsap from "gsap"
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+
 // scene
 const scene = new THREE.Scene();
 
@@ -10,16 +15,16 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-// sizes
+//sizes
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
-};
+}
 
 // Point Light
 const pointLight = new THREE.PointLight(0xffffff, 30);
 pointLight.position.set(-4, 4, 4);
-pointLight.intensity = 1.25;
+pointLight.intesity = 1.25
 scene.add(pointLight);
 
 // Camera
@@ -28,60 +33,64 @@ camera.position.z = 15;
 scene.add(camera);
 
 // Renderer
-const canvas = document.querySelector('.webgl');
+const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(4);
+renderer.setPixelRatio(4)
 
-// Controls
-const controls = new THREE.OrbitControls(camera, canvas);
-controls.enableDamping = true;
-controls.enablePan = false;
-controls.enableZoom = false;
-controls.autoRotate = true;
-controls.autoRotateSpeed = 5;
+// controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping =  true
+controls.enablePan = false
+controls.enableZoom = false
+controls.autoRotate = true
+controls.autoRotateSpeed = 5
 
-// Resize
+// resize
 window.addEventListener('resize', () => {
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+  //update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-  renderer.setSize(sizes.width, sizes.height);
-});
+  //update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(sizes.width, sizes.height)
+})
 
-// Animation loop
+
 const loop = () => {
-  controls.update();
-  renderer.render(scene, camera);
-  window.requestAnimationFrame(loop);
-};
-loop();
+  controls.update()
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(loop)
+}
+loop()
 
-// Timeline animations with gsap
-const tl = gsap.timeline({ defaults: { duration: 1 } });
-tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
-tl.fromTo('nav', { y: "-100%" }, { y: "0%" });
-tl.fromTo('.title', { opacity: 0 }, { opacity: 1 });
+// timeline magic
+const tl = gsap.timeline({ defaults: {duration: 1 } })
+  tl.fromTo(mesh.scale, {z:0, x:0, y:0}, {z:1, x:1, y:1})
+  tl.fromTo('nav', {y: "-100%"}, { y: "0%" })
+  tl.fromTo('.title', { opacity: 0}, {opacity: 1 })
 
-// Mouse animation color
-let mouseDown = false;
-let rgb = [];
-window.addEventListener("mousedown", () => (mouseDown = true));
-window.addEventListener("mouseup", () => (mouseDown = false));
-window.addEventListener('mousemove', (e) => {
-  if (mouseDown) {
-    rgb = [
-      Math.round((e.pageX / sizes.width) * 255),
-      Math.round((e.pageY / sizes.height) * 255),
-      150,
-    ];
-    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
-    gsap.to(mesh.material.color, {
-      r: newColor.r,
-      g: newColor.g,
-      b: newColor.b,
-    });
-  }
-});
+  // mouse animation color
+  let mouseDown = false
+  let rgb = [];
+  window.addEventListener("mousedown", () => (mouseDown = true))
+  window.addEventListener("mouseup", () => (mouseDown = false))
+  window.addEventListener('mousemove', (e) => {
+    if(mouseDown){
+      rgb = [
+        Math.round((e.pageX / sizes.width) * 255),
+        Math.round((e.pageY / sizes.height) * 255),
+        150,
+      ]
+      console.log(rgb)
+      // animate <3
+      let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+        gsap.to(mesh.material.color, {
+          r: newColor.r,
+          g: newColor.g,
+          b: newColor.b,
+        })
+    }
+  })
